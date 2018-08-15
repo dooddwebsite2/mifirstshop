860e9cb72dc9f01a8330b8df8dedd5ef645451c8
bankben2005 <bankben2005>
2018-08-16 03:14:33 +0700

    remote div fb

--- contact.php
+++ contact.php
@@ -182,59 +182,7 @@
 
     </div>
 
-    <!-- <script>
-            window.fbAsyncInit = function() {
-              FB.init({
-                appId      : '95100348886',
-                xfbml      : true,
-                version    : 'v2.6'
-              });
-            };
-      
-            (function(d, s, id){
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) {return;}
-              js = d.createElement(s); js.id = id;
-              js.src = "//connect.facebook.net/en_US/sdk.js";
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-          </script>
-      
-          <div class="fb-page" 
-               data-href="https://www.facebook.com/225089078349628" 
-               data-tabs="messages" 
-               data-width="400" 
-               data-height="300" 
-               data-small-header="true">
-            <div class="fb-xfbml-parse-ignore">
-              <blockquote></blockquote>
-            </div>
-          </div> -->
-
-
-    <script>
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: '225089078349628',
-                xfbml: true,
-                version: 'v2.6'
-            });
-        };
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-
-    <div class="fb-messengermessageus" messenger_app_id="225089078349628" page_id="XYZ" color="blue" size="large">
-    </div>
+    
 </body>
 
 <script>
